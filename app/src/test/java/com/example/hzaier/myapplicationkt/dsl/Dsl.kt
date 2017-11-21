4cd9e72a7d58adc092a3cf81b8613b0590633ecb
Houssem Zaier <houssem.zaier@gmail.com>
2017-11-21 09:34:20 +0100

    DSL is OK

--- app/src/test/java/com/example/hzaier/myapplicationkt/dsl/Dsl.kt
+++ app/src/test/java/com/example/hzaier/myapplicationkt/dsl/Dsl.kt
@@ -1,9 +1,84 @@
 package com.example.hzaier.myapplicationkt.dsl
 
-/**
- * Created by houssem on 20/11/2017.
- * https://www.lynda.com/Java-tutorials/DSL-construction/562926/635728-4.html?srchtrk=index%3a1%0alinktypeid%3a2%0aq%3akotlin%0apage%3a1%0as%3arelevance%0asa%3atrue%0aproducttypeid%3a2
- *
- */
-interface Dsl {
+import org.junit.Test
+
+
+interface Matcher<T> {
+    fun test(lhs: T)
+
+    infix fun or(other: Matcher<T>): Matcher<T> = object : Matcher<T> {
+        override fun test(lhs: T) {
+            try {
+                this@Matcher.test(lhs)
+            } catch (e: RuntimeException) {
+                other.test(lhs)
+            }
+        }
+    }
+
+
+}
+
+infix fun <T> T.should(matcher: Matcher<T>) {
+    matcher.test(this)
+}
+
+object StartsWithWhiteSpace : Matcher<String> {
+    override fun test(lhs: String) {
+        if (!lhs.startsWith(" "))
+            throw RuntimeException("doesn't start with WhiteSpace")
+    }
+}
+
+object EndsWithWhiteSpace : Matcher<String> {
+    override fun test(lhs: String) {
+        if (!lhs.endsWith(" "))
+            throw RuntimeException("doesn't ends with WhiteSpace")
+    }
+}
+
+
+infix fun <T> Collection<T>.should(fn: CollectionMatcher<T>.() -> Unit) {
+    CollectionMatcher(this).fn()
+}
+
+class CollectionMatcher<T>(private val collection: Collection<T>) {
+
+    fun contains(rhs: T): CollectionMatcher<T> {
+        if (!collection.contains(rhs)) throw RuntimeException("Collection did not contains $rhs")
+        return this
+    }
+
+    fun notContains(rhs: T): CollectionMatcher<T> {
+        if (collection.contains(rhs)) throw RuntimeException("Collection did not contains $rhs")
+        return this
+    }
+
+    fun haveSizeLessThan(size: Int): CollectionMatcher<T> {
+        if (collection.size >= size) throw RuntimeException("Collection should have size $size")
+        return this
+    }
+}
+
+infix fun <T> CollectionMatcher<T>.and(lhs: CollectionMatcher<T>): CollectionMatcher<T> = this
+
+class Test {
+
+    @Test
+    fun test() {
+        val listOfNames = listOf("A", "C", "B", "D")
+        listOfNames should {
+            contains("A") and contains("D") and notContains("z") and haveSizeLessThan(5)
+         }
+
+        "ssss " should (StartsWithWhiteSpace or EndsWithWhiteSpace)
+
+
+        //"Z " should object : Matcher<String> {
+        //    override fun test(lhs: String) {
+        //        print("lhd $lhs")
+        //   }
+        //}
+        print("DONE")
+    }
 }
